f7551fe908d366ec126c488a208fa661e05c76d8
Pratik Gohil <pr.tikgohil@gmail.com>
2022-03-09 03:16:01 +0530

    fixed: port number

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
