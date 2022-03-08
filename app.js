200ea48633e12b2be32c5cb5fbeea522ab8153f3
Pratik Gohil <pr.tikgohil@gmail.com>
2022-03-09 03:00:49 +0530

    publishing to heroku

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Todo = require("./models/todos");
+
+const uri =
+  "mongodb+srv://pratik:v4hGtC353oYCzTiu@cluster0.vugkh.mongodb.net/todo_app?retryWrites=true&w=majority";
+mongoose
+  .connect(uri)
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log(err));
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  Todo.find()
+    .sort({ createdAd: -1 })
+    .then((todos) => {
+      res.render("index", { title: "Todos", todos });
+    });
+});
+
+app.get("/todos", (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.send(todos);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.post("/todo", (req, res) => {
+  const todo = new Todo(req.body);
+
+  todo
+    .save()
+    .then((result) => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
+app.delete("/todo/:id", (req, res) => {
+  const id = req.params.id;
+
+  Todo.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/" });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use((req, res) => {
+  res.render("404", { title: "Page Not Found" });
+});
